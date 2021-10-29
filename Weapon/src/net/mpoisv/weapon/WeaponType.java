3af1e9b1b7745b57ff9ee1bd9fdbaba653881978
skyneton <67633420+skyneton@users.noreply.github.com>
2021-10-29 19:39:57 +0900

    DELETE

--- Weapon/src/net/mpoisv/weapon/WeaponType.java
+++
@@ -1,6 +0,0 @@
-package net.mpoisv.weapon;
-
-public enum WeaponType {
-	MAIN,
-	SECOND
-}
--- Weapon/src/net/mpoisv/weapon/player/WeaponInfo.java
+++
@@ -1,9 +0,0 @@
-package net.mpoisv.weapon.player;
-
-import net.mpoisv.weapon.Weapon;
-
-public class WeaponInfo {
-    public Weapon weapon;
-    public long lastShootTime;
-    public int bulletAmount;
-}
